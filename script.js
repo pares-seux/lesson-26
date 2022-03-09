e0b166de84f233652902b69ab56c0c173dd47626
pares-seux <89102668+pares-seux@users.noreply.github.com>
2022-03-09 08:22:38 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 const sendData = (destination, dataForSend) => {
-  console.log(dataForSend);
   return fetch(destination, {
     method: "POST",
     headers: {
@@ -7,8 +6,9 @@ const sendData = (destination, dataForSend) => {
     },
     body: JSON.stringify(dataForSend),
   })
-    .then((response) => console.log(response))
-    .catch((error) => console.log(error));
+  .then((response) => response.json())
+  .then((data) => console.log(data))
+  .catch((error) => console.log(error));
 };
 
 const getData = (sourse) => {
